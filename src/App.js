ac824db67ac7babc2add86f11c1af761aeb2d705
zhemdbs <zhemdbs@gmail.com>
2021-08-19 22:56:21 +0900

    return commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello!!!!!!!!React Practice</h1>
-    </div>
-  );
+import React from 'react';
+import PropTypes from "prop-types";
+
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button>Add</button>
+        <button>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
