6797b5d66bd056f868c11d857d9fec8c02d6aa81
zhemdbs <zhemdbs@gmail.com>
2021-08-20 23:13:53 +0900

    add, minus button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ class App extends React.Component{
   state = {
     count: 0
   };
+  add = () => {
+    console.log('add');
+  };
+  minus = () => {
+    console.log('minus');
+  };
   render() {
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
-        <button>Add</button>
-        <button>Minus</button>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
