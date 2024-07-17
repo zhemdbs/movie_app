3334fc14d048d1aa8d9fd9a81b2d7f01bc6b6aa9
zhemdbs <zhemdbs@gmail.com>
2024-07-17 15:21:17 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from "./routes/Home";
 function App() {
   return <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail />} />
-      <Route path="/" element={<Home />} />
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={Detail} />
+      <Route path={`${process.env.PUBLIC_URL}/`} element={Home} />
     </Routes>
   </Router>;
 }
